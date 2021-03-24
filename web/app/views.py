09c49eaa47a5e7967083162ba3566128f31d9688
Tanvi Shinde <tanvi.shinde46@gmail.com>
2021-03-24 14:04:23 +0530

    Rerouting for login and dashboard

--- web/app/views.py
+++ web/app/views.py
@@ -1,6 +1,4 @@
 # -*- encoding: utf-8 -*-
-
-
 # Flask modules
 from flask import render_template, request
 from jinja2 import TemplateNotFound
@@ -9,32 +7,26 @@ from jinja2 import TemplateNotFound
 from app import app
 
 # App main route + generic routing
-@app.route('/', defaults={'path': 'index.html'})
+@app.route('/', methods=['POST', 'GET']) #defaults={'path': 'login.html'}
+def home():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        return render_template('index.html')
+
 @app.route('/<path>')
 def index(path):
-
     try:
-
-        # Detect the current page
         segment = get_segment( request )
-
-        # Serve the file (if exists) from app/templates/FILE.html
         return render_template( path, segment=segment )
-
     except TemplateNotFound:
         return render_template('page-404.html'), 404
 
-# Helper - Extract current page name from request 
-def get_segment( request ): 
-
+def get_segment(request): 
     try:
-
         segment = request.path.split('/')[-1]
-
         if segment == '':
             segment = 'index'
-
-        return segment    
-
+        return segment
     except:
         return None   
